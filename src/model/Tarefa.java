d0732705e3b1598ea772579a1d0be42c52bf892f
raulcamurca <raulcamurca22@gmail.com>
2026-07-08 15:04:39 -0300

    feat: classe tarefa

---
+++ src/model/Tarefa.java
@@ -0,0 +1,61 @@
+package model;
+
+public class Tarefa {
+    private int id;
+    private String titulo;
+    private String descricao;
+    private boolean concluida;
+
+    public Tarefa(int id, String titulo, String descricao) {
+        this.id = id;
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.concluida = false;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId (int id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public boolean isConcluida() {
+        return concluida;
+    }
+
+    public void setConcluida(boolean concluida) {
+        this.concluida = concluida;
+    }
+
+    @Override
+    public String toString() {
+        String status;
+
+        if (concluida) {
+            status = "Concluida";
+        } else {
+            status = "Pendente";
+        }
+        return "ID" + id + "Título: " + titulo + "Descrição: " + descricao + "Status: " + status;
+    }
+}
+
+
