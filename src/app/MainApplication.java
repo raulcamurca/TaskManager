f7350d77010dcdeaca63f6b7b81da9dd657f7451
raulcamurca <raulcamurca22@gmail.com>
2026-09-06 00:33:59 -0300

    feat: pasta view + tela principal e javafx importado

---
+++ src/app/MainApplication.java
@@ -0,0 +1,30 @@
+package app;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainApplication extends Application {
+    
+    @Override 
+    public void start(Stage stage) throws Exception {
+
+        FXMLLoader loader = new FXMLLoader(
+            getClass().getResource("/view/Principal.fxml")
+        );
+
+        Scene scene = new Scene(loader.load());
+
+        stage.setTitle(STYLESHEET_CASPIAN);
+        stage.setScene(scene);
+        stage.setMinWidth(0);
+        stage.setMinHeight(0);
+        stage.show();
+    }
+
+    public static void main (String[] args) {
+        launch(args);
+    }
+
+}
